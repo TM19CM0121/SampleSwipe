e36db1ef7a83389d70cf451e8db522f4ad99e64e
otakara <19cm0121@jec.ac.jp>
2021-02-26 10:43:27 +0900

    Initial Commit

---
+++ SampleSwipe/SampleSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwipeApp.swift
+//  SampleSwipe
+//
+//  Created by 高良 昌辰 on 2021/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
